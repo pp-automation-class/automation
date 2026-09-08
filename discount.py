2756b0718297d99261bf8bd51ef39d833123157a
Denis <denis5gribenyuk@gmail.com>
2026-09-09 00:20:07 +1000

    This is my homework for the first week (training to create a calculator, discount calculator, and more) On branch Denis-Python Changes to be committed: 	new file:   calculator.py 	new file:   celsius_to_fahrenheit.py 	new file:   discount.py 	new file:   main.py 	new file:   pyproject.toml 	new file:   src/automation/__init__.py 	new file:   uv.lock 	new file:   variables.py

---
+++ discount.py
@@ -0,0 +1,27 @@
+age = int(input("Enter your age: "))
+# for 18 and less discount is 10%
+# for more than 18 to 64 discount is 5%
+# for 60 and more discount is 15%
+
+if age <= 18:
+    print(10)
+if age > 18 and age > 65:
+    print(5)
+else:
+    print(15)
+or
+and
+
+#And
+True and True = True
+True and False = False
+False and True = False
+False and False = False
+
+#Or
+True or True = True
+True or False = True
+False or True = True
+False or False = False
+
+#Not
---
+++ variables.py
@@ -0,0 +1,53 @@
+APP_NAME = "www.google.com"
+
+# str — text
+APP_URL = "www.google.com"
+username = "admin"
+url = APP_NAME
+
+# int — whole number
+port = 443
+retry_count = 3
+
+# float — decimal number
+timeout_seconds = 2.5
+price = 9.99
+
+# bool — True or False
+is_logged_in = False
+debug = True
+
+a = int("10")
+b = 2
+print(type(a))
+print(type(b))
+
+# # None — empty / no value
+# error = None
+
+# # list — ordered collection
+# browsers = ["chromium", "firefox", "webkit"]
+# roles = ["user", "admin"]
+
+# # tuple — ordered, cannot change
+# credentials = ("admin", "secret")
+
+# # dict — key/value pairs
+# user = {
+#     "name": username,
+#     "active": is_logged_in,
+#     "retries": retry_count,
+# }
+
+# # set — unique values only
+# unique_roles = {"user", "admin", "user"}
+
+# print(type(APP_NAME), APP_NAME)
+# print(type(port), port)
+# print(type(timeout_seconds), timeout_seconds)
+# print(type(is_logged_in), is_logged_in)
+# print(type(error), error)
+# print(type(browsers), browsers)
+# print(type(credentials), credentials)
+# print(type(user), user)
+# print(type(unique_roles), unique_roles)

56baf2a5576df75d9980af8ed72b182e3e75dac1
Yulia <yuliatim3@mail.ru>
2026-09-08 19:00:28 -0500

    chande EK name

--- C_to_F_temperature.py
+++
@@ -1,9 +0,0 @@
-print("Celsius to Fahrenheit")
-
-celsius = float(input("Enter temperature in C: "))
-
-if celsius < 20 or celsius > 40:
-    print("out of range")
-else:
-    fahrenheit = celsius * 9 / 5 + 32
-    print("F:", fahrenheit)
